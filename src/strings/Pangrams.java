df85f14d3d0bbada9675f4f7485cd59e81bd6bb7
Tamilthaaragai Muthukumar <tmuthuk@ncsu.edu>
2015-08-15 17:01:03 -0400

    Added Problem statement

--- src/strings/Pangrams.java
+++ src/strings/Pangrams.java
@@ -3,6 +3,28 @@ package strings;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/***
+ * 
+ * @author Tamilthaaragai
+ *Problem Statement
+
+Roy wanted to increase his typing speed for programming contests. 
+So, his friend advised him to type the sentence "The quick brown fox jumps over the lazy dog" repeatedly, because it is a pangram. (Pangrams are sentences constructed by using every letter of the alphabet at least once.)
+
+After typing the sentence several times, Roy became bored with it. So he started to look for other pangrams.
+
+Given a sentence s, tell Roy if it is a pangram or not.
+
+Input Format Input consists of a line containing s.
+
+Constraints 
+Length of s can be at most 103 (1≤|s|≤103) and it may contain spaces, 
+lower case and upper case letters. Lower case and upper case instances of a letter are considered the same.
+
+Output Format Output a line containing pangram if s is a pangram, 
+otherwise output not pangram.
+ */
+
 public class Pangrams {
 
 	public static void main(String[] args) {
