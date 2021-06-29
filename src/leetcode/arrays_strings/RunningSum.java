677948139eb9520d9279ac1e295269701ab34473
tmuthuk <tmuthuk@ncsu.edu>
2021-06-28 22:16:09 -0700

    running sum problem

---
+++ src/leetcode/arrays_strings/RunningSum.java
@@ -0,0 +1,43 @@
+package leetcode.arrays_strings;
+
+/**
+ * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ *
+ * Return the running sum of nums.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,4]
+ * Output: [1,3,6,10]
+ * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+ * Example 2:
+ *
+ * Input: nums = [1,1,1,1,1]
+ * Output: [1,2,3,4,5]
+ * Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+ * Example 3:
+ *
+ * Input: nums = [3,1,2,10,1]
+ * Output: [3,4,6,16,17]
+ */
+public class RunningSum {
+    public static int[] runningSum(int[] nums) {
+
+        int[] retVal = new int[nums.length];
+        retVal[0] = nums[0];
+
+        for(int i = 1; i< nums.length ; i++) {
+            retVal[i] = retVal[i-1] + nums[i];
+        }
+
+        return retVal;
+    }
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,2,3,4};
+        int [] nums2 = new int[]{3,1,2,10,1};
+        runningSum(nums1);
+        runningSum(nums2);
+    }
+}
