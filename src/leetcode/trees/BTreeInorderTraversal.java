d550c44bc119f36b1271be66999f4e35f76ea4c1
tmuthuk <tmuthuk@ncsu.edu>
2021-07-07 22:24:42 -0700

    Btree Inorder traversal

---
+++ src/leetcode/trees/BTreeInorderTraversal.java
@@ -0,0 +1,30 @@
+package leetcode.trees;
+
+import leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LC easy #94
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ */
+public class BTreeInorderTraversal {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        traverse(root);
+        return list;
+    }
+
+    public void traverse(TreeNode node) {
+        if(node == null) return;
+        System.out.println("curr node " + node.val);
+        traverse(node.left);
+        list.add(node.val);
+        traverse(node.right);
+
+    }
+}
